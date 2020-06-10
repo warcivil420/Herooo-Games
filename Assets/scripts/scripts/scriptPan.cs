91a3edda99422daadcd349b4411e17dbb1583b6f
Herooo Games <pixtergoood@gmail.com>
2020-06-10 05:43:21 +0500

    new Script

---
+++ Assets/scripts/scripts/scriptPan.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptPan : MonoBehaviour
+{
+   [SerializeField]
+   	GameObject Panel;
+   
+   	void Start(){
+   		Panel.SetActive(false);
+   	}
+
+   	void OnTriggerEnter2D(Collider2D col)
+   	{
+
+   		if (col.tag == "Player")
+   		{
+
+   			Panel.SetActive(true);
+   			Time.timeScale = 0.1f*Time.deltaTime;
+
+   		}
+
+   	}
+
+
+}
